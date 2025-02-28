fadd4ea95633b060016eb0e04385833f00137c65
Helenazhx <zhx9225@gmail.com>
2025-02-28 17:05:44 +0800

    add para isSBS

--- Sources/Models/StreamModel.swift
+++ Sources/Models/StreamModel.swift
@@ -21,6 +21,9 @@ public struct StreamModel: Codable {
     public var forceFieldOfView: Float?
     /// True if the media required user permission for access.
     public var isSecurityScoped: Bool
+    /// True if is 3D video
+    public var isSBS: Bool
+    
     
     /// Public initializer for visibility.
     /// - Parameters:
@@ -30,13 +33,14 @@ public struct StreamModel: Codable {
     ///   - fallbackFieldOfView: the fallback horizontal field of view of the video, if cannot be determined from the media, in degrees (default 180.0).
     ///   - forceFieldOfView: optional forced horizontal field of view of the video, if needed to override the value encoded in the media, in degrees (default nil).
     ///   - isSecurityScoped: true if the media required user permission for access (default false).
-    public init(title: String, details: String, url: URL, fallbackFieldOfView: Float = 180.0, forceFieldOfView: Float? = nil, isSecurityScoped: Bool = false) {
+    public init(title: String, details: String, url: URL, fallbackFieldOfView: Float = 180.0, forceFieldOfView: Float? = nil, isSecurityScoped: Bool = false, isSBS: Bool = false) {
         self.title = title
         self.details = details
         self.url = url
         self.fallbackFieldOfView = fallbackFieldOfView
         self.forceFieldOfView = forceFieldOfView
         self.isSecurityScoped = isSecurityScoped
+        self.isSBS = isSBS
     }
 }
 
